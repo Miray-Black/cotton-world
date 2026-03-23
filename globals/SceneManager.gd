e4c64e198172fc9456ce68a3db07fe2511b19da9
Miray <1lonelybrony1@gmail.com>
2026-03-23 19:03:53 +0300

    Update 13-230626

--- globals/SceneManager.gd
+++ globals/SceneManager.gd
@@ -21,13 +21,18 @@ func load_scene(path: String, use_loading_screen: bool = false):
 
 func _update_hud_visibility():
 	var current = get_tree().current_scene
-	if current.is_in_group("lvls"):
+	if current.is_in_group("lvls"): 
 		HUD.show_hud()
 	else:
 		HUD.hide_hud()
 
 # Вернуться в главное меню
 func go_to_main_menu():
+	# Сбрасываем всё, чтобы при новом старте было чисто
+	Global.inventory.clear()
+	Global.desires_count = 0
+	Global.enemies_killed = 0
+	Global.player_health = Global.player_max_health
 	load_scene(MAIN_MENU_PATH)
 
 # Начать новую игру (сбросить глобальные данные и загрузить первый уровень)
